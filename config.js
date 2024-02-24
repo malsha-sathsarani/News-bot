23478da5b085d5fbd2b6bf998003e998e3a370a3
DarkWinzo <darkwinzo4440@gmail.com>
2024-02-24 17:44:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,7 @@ module.exports = {
   USER_NAME: "user1", //Enter Your UserName
   PASSWORD: "password1", //Enter Your Password
   GROUP_JID: [
-    "120363161816871173@g.us",
-    "120363144403811486@g.us",
-    "120363162110554625@g.us",
-    "120363185730569877@g.us",
+    "",
+    ""
   ] //News Group Jid
 };
